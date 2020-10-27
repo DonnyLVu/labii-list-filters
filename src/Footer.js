25960acd1a892258fac9cfd741b1773ce59af13b
DonnyLVu <donnyvu897@gmail.com>
2020-10-27 15:49:28 -0700

    added Header, Data, Footer, ImageItem, ImageList

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>HI IM THE FOOTER</h2>
+            </div>
+        )
+    }
+}
---
+++ src/ImageList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import images from './Data.js';
+import ImageItem from './ImageItem.js';
+
+export default class ImageList extends React.Component {
+    render() {
+        return (
+            <div>
+                {images.map(image => {
+                    return <ImageItem 
+                    title={image.title}
+                    url={image.url}
+                    description={image.description}
+                    horns={image.horns}
+                    keyword={image.keyword}/>
+                })}
+            </div>
+        )
+    }
+}
