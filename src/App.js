3f0aa7e552adb98a28b47afd89b266621cc19d5e
DonnyLVu <donnyvu897@gmail.com>
2020-10-28 13:25:53 -0700

    did what we did in class

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import './App.css';
 import React from 'react';
 import images from './Data.js';
-import Header from './Header';
-import Footer from './Footer';
-import ImageList from './ImageList';
+import DropDown from './DropDown.js';
+import Header from './Header.js';
+// import Footer from './Footer'
+import ImageList from './ImageList.js';
 
 export default class App extends React.Component {
+state = {
+  filter: '',
+  horns: '',
+  cool: '',
+}
+handleChange = e => {
+  this.setState({
+      filter: e.target.value,
+  });
+}
+handleChangeHorns = e => {
+    this.setState({
+        horns: e.target.value,
+    });
+}
+handleChangeReal = e => {
+  this.setState({
+      real: e.target.value,
+  });
+}
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <ImageList imagesProp={images} />
-        <Footer className='footer'/>
-      </div>
-    )
-  }
+render() {
+  return (
+    <div>
+      <Header />
+      <DropDown handleChange={this.handleChange} handleChangeHorns={this.handleChangeHorns} handleChangeReal={this.handleChangeReal} />
+      <ImageList imagesProp={images} horns={this.state.horns} filter={this.state.filter} size={this.state.size} real={this.state.real}/>
+    </div>
+  )
+}
 }
--- src/ImageList.js
+++ src/ImageList.js
@@ -3,81 +3,26 @@ import React from 'react';
 import ImageItem from './ImageItem.js';
 
 export default class ImageList extends React.Component {
-    
-    state = {
-        filter: '',
-        horns: '',
-        real: '',
-    }
-
-    handleChange = e => {
-        this.setState({
-            filter: e.target.value,
-        });
-    }
-
-    handleChangeHorns = e => {
-        this.setState({
-            horns: e.target.value,
-        });
-    }
-
-    handleChangeReal = e => {
-        this.setState({
-            real: e.target.value,
-        });
-    }
-
-
     render() {  
         return (
             <>
-                <div className= 'drop-down'>
-                    <select onChange={this.handleChange}>
-                        <option value=''>SHOW EVERYTHING</option>
-                        <option value='narwhal'>Narwhal</option>
-                        <option value='rhino'>Rhino</option>
-                        <option value='unicorn'>Unicorn</option>
-                        <option value='unilego'>Unilego</option>
-                        <option value='triceratops'>Triceratops</option>
-                        <option value='markhor'>Markhor</option>
-                        <option value='mouflon'>Mouflon</option>
-                        <option value='addax'>Addax</option>
-                        <option value='chameleon'>Chameleon</option>
-                        <option value='lizard'>Lizard</option>
-                        <option value='dragon'>Dragon</option>
-                    </select>
-                    <select onChange={this.handleChangeHorns}>
-                        <option value=''>SHOW ALL THE HORNS</option>
-                        <option value='1'>1</option>
-                        <option value='2'>2</option>
-                        <option value='3'>3</option>
-                        <option value='100'>100</option>
-                    </select>
-                    <select onChange={this.handleChangeReal}>
-                        <option value=''>LIVING OR NONLIVING? DONT CARE!</option>
-                        <option value='True'>True they are really "living"</option>
-                        <option value='False'>False they are not real "living"</option>
-                    </select>
-
-                </div>
-                <div className= 'whole-group'>
+                <div className= 'entireCollection'>
                     {
                         this.props.imagesProp.filter((image) => {
-                            if (!this.state.filter) return true;
-                            if (image.keyword === this.state.filter) return true;
+                            if (!this.props.filter) return true;
+                            if (image.keyword === this.props.filter) return true;
                             return false;
                           })
 
                         .filter((image) => {
-                            if (!this.state.horns) return true;
-                            if (image.horns === +this.state.horns) return true;
+                            if (!this.props.horns) return true;
+                            if (image.horns === +this.props.horns) return true;
                             return false;
                           })
 
                         .filter((image) => {
-                            if (!this.state.real) return true;
-                            if (image.real === this.state.real) return true;
+                            if (!this.props.real) return true;
+                            if (image.real === this.props.real) return true;
                             return false;
                           })
                     
